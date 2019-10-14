31bc33d169ce8868516da8e67b85397d8fa90cfa
Eric Taylor <ericfromspringfield@gmail.com>
2019-10-14 14:12:00 -0500

    Completed Using Custom Types

--- Program.cs
+++ Program.cs
@@ -86,18 +86,91 @@ namespace linq2
     879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
 };
 
-        }
-    }
-}
+            double maxPrice = prices.Max();
+            Console.WriteLine(maxPrice);
 
-//     nums.TakeWhile(num => 
-//     {
-//         //return true if i is NOT a perfect square
-//         //return false if i IS a perfect square
-//         double sqrt = Math.Sqrt(num);
-//         return sqrt % 1 != 0; 
-//     }).ToList().ForEach(Console.WriteLine);
-// }
+            /*
+    Store each number in the following List until a perfect square
+    is detected.
+
+    Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+*/
+            List<int> wheresSquaredo = new List<int>()
+{
+    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+};
+
+    wheresSquaredo.TakeWhile(num => 
+    {
+        //return true if i is NOT a perfect square
+        //return false if i IS a perfect square
+        double sqrt = Math.Sqrt(num);
+        return sqrt % 1 != 0; 
+    }).ToList().ForEach(Console.WriteLine);
+        List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+          
+       var millionaireCustomers = customers
+        .Where(c => c.Balance <= 500000)
+        .GroupBy(customer => customer.Bank);
+
+        foreach (var customerGroup in millionaireCustomers)
+        {
+         Console.WriteLine($"{customerGroup.Key} {customerGroup.Count()}");
+         foreach (var customer in customerGroup)
+         {
+             Console.WriteLine(customer.Name);
+         }
+        }
+
+        }
+        // Build a collection of customers who are millionaires 
+        //Millionaires per bank
+        
+public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
+                
+       
+    }
+        }
+    
+        
+
+    
+    
+    
+
+
+/*
+    Given the same customer set, display how many millionaires per bank.
+    Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+    Example Output:
+    WF 2
+    BOA 1
+    FTB 1
+    CITI 1
+*/
+
+
+    
+
+
+
 
 
 
